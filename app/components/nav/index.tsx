e525afc8cc2b15ee6bdb9cc66822c22fc2c5f14f
caicheng <caicheng@bytedance.com>
2023-08-03 18:53:46 +0800

    feat: starter for site

---
+++ app/components/nav/index.tsx
@@ -0,0 +1,14 @@
+
+const TopNav = () => {
+  return <div className="w-full h-12 br">
+    Aeptom
+Bedding
+Inserts
+Bundles
+Blanket
+Care Guide
+Bedding QuizSale
+  </div>;
+};
+
+export default TopNav;
